2c2a88e8f8f458702f7fed8c276e2dcd9ef43b8d
pradyumna <pradyumnasp1234@gmail.com>
2022-08-19 16:27:03 +0530

    fisrt

--- SquareRoot.java
+++ SquareRoot.java
@@ -1,24 +1,13 @@
-import java.util.Scanner;  
+
 public class SquareRoot  {  
 	public static void main(String[] args) {   
 		System.out.print("Enter a number: ");  
 		 
-		Scanner sc = new Scanner(System.in);  
+	 
 		  
-		int n = sc.nextInt();  
+		int n = 10;  
 	
-		System.out.println("The square root of "+ n+ " is: "+squareRoot(n));  
+		System.out.println("The square root of "+ n);  
 	}  
-	public static double squareRoot(int num) {  
-		
-		double t;  
-		double sqrtroot=num/2;  
-		do   
-		{  
-			t=sqrtroot;  
-			sqrtroot=(t+(num/t))/2;  
-		}   
-		while((t-sqrtroot)!= 0);  
-		return sqrtroot;  
-	}  
+	
 }
